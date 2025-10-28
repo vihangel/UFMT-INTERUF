fadcc45ef94778780c4d774414cc6748b0b6b68e
Angel <vitoria.angel2002@gmail.com>
2025-10-28 11:48:26 -0400

    lojas

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         applicationId = "io.interufmt"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
