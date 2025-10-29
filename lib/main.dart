a6ebc864dcc7ce5041fbf875b6d5f7e878eb1855
Sergio Pinheiro <sergiopinheiro_2002@outlook.com>
2025-10-29 11:30:31 -0400

    trojan_loading

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:interufmt/core/config/url_strategy.dart';
 import 'package:interufmt/firebase_options.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
-
+import 'package:interufmt/features/landing/branding_screen.dart';
 import 'core/config/environment.dart';
 import 'core/data/services/local_storage_service.dart';
 import 'core/di/app_module.dart';
@@ -54,14 +54,39 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   late GoRouter appRoutes;
+  bool _isShowingBranding = true; // 1. Controla o estado
+
   @override
   void initState() {
     super.initState();
-    appRoutes = AppRoutes.getRouter(context, navigatorKey, widget.initialRoute);
+
+    // 2. Inicia o timer de 3 segundos
+    Timer(const Duration(seconds: 3), () {
+      // 3. Após 3s, inicializa o GoRouter...
+      appRoutes = AppRoutes.getRouter(
+        context,
+        navigatorKey,
+        widget.initialRoute,
+      );
+
+      // 4. ...e manda o build() reconstruir, agora mostrando o app principal
+      if (mounted) {
+        setState(() {
+          _isShowingBranding = false;
+        });
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    // 5. Lógica de exibição
+    if (_isShowingBranding) {
+      // Se ainda estivermos no tempo da branding, mostre a tela
+      return const BrandingScreen();
+    }
+
+    // 6. Quando o tempo acabar, mostre seu app principal com GoRouter
     return MaterialApp.router(
       title: 'InterUFMT',
       theme: AppTheme.theme,
